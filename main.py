24aa2ad1f884950f72bd670e94713f44e45b49ba
luuvmane <162039016+luuvmane@users.noreply.github.com>
2024-04-14 18:58:57 +0300

    ДЗ5

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+from abc import ABC, abstractmethod
+
+class LogMixin:
+    def __repr__(self):
+        attributes = ', '.join(f"{key}={value}" for key, value in self.__dict__.items())
+        return f"{self.__class__.__name__}({attributes})"
+
+class AbstractProduct(ABC, LogMixin):
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+    @abstractmethod
+    def __str__(self):
+        """Абстрактный метод для строкового представления продукта."""
+        pass
+
+    @abstractmethod
+    def __add__(self, other):
+        """Абстрактный метод для сложения продуктов."""
+        pass
+
 class Category:
     total_categories = 0
     unique_products = set()
@@ -10,8 +34,8 @@ class Category:
 
     def add_product(self, product):
         """Метод для добавления товара в категорию."""
-        if not isinstance(product, (Product, Smartphone, Grass)):
-            raise TypeError("Можно добавлять только объекты типа Product и его наследников")
+        if not isinstance(product, AbstractProduct):
+            raise TypeError("Можно добавлять только объекты типа AbstractProduct и его наследников")
 
         self.__products.append(product)
         Category.unique_products.add(product.name)
@@ -35,39 +59,9 @@ class Category:
         """Магический метод для строкового представления категории."""
         return f"{self.name}, количество продуктов: {len(self)} шт."
 
-
-class Product:
-    def __init__(self, name, description, price, quantity):
-        self.name = name
-        self.description = description
-        self.price = price
-        self.quantity = quantity
-
-    @staticmethod
-    def create_product(name, description, price, quantity):
-        """Статический метод для создания товара и возвращения объекта товара."""
-        return Product(name, description, price, quantity)
-
-    @property
-    def price(self):
-        """Геттер для цены товара."""
-        return self.__price
-
-    @price.setter
-    def price(self, new_price):
-        """Сеттер для цены товара."""
-        if new_price >= 0:
-            self.__price = new_price
-        else:
-            raise ValueError("Цена не может быть отрицательной")
-
-    @price.deleter
-    def price(self):
-        """Делитер для цены товара."""
-        del self.__price
-
+class Product(AbstractProduct):
     def __str__(self):
-        """Магический метод для строкового представления товара."""
+        """Метод для строкового представления товара."""
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
     def __add__(self, other):
@@ -78,7 +72,7 @@ class Product:
         total_price = (self.price * self.quantity + other.price * other.quantity)
         return Product("Сумма товаров", "Сумма товаров", total_price, total_quantity)
 
-class Smartphone(Product):
+class Smartphone(AbstractProduct):
     def __init__(self, name, description, price, quantity, performance, model, storage_capacity, color):
         super().__init__(name, description, price, quantity)
         self.performance = performance
@@ -89,8 +83,15 @@ class Smartphone(Product):
     def __str__(self):
         return f"{self.name}, {self.model}, {self.color}, {self.price} руб. Остаток: {self.quantity} шт."
 
+    def __add__(self, other):
+        if type(self) != type(other):  # Проверяем типы объектов
+            raise TypeError("Невозможно сложить товары разных классов")
 
-class Grass(Product):
+        total_quantity = self.quantity + other.quantity
+        total_price = (self.price * self.quantity + other.price * other.quantity)
+        return Smartphone("Сумма товаров", "Сумма товаров", total_price, total_quantity, "", "", "", "")
+
+class Grass(AbstractProduct):
     def __init__(self, name, description, price, quantity, country_of_origin, sprouting_period, color):
         super().__init__(name, description, price, quantity)
         self.country_of_origin = country_of_origin
@@ -100,14 +101,25 @@ class Grass(Product):
     def __str__(self):
         return f"{self.name}, {self.country_of_origin}, {self.color}, {self.price} руб. Остаток: {self.quantity} шт."
 
+    def __add__(self, other):
+        if type(self) != type(other):  # Проверяем типы объектов
+            raise TypeError("Невозможно сложить товары разных классов")
+
+        total_quantity = self.quantity + other.quantity
+        total_price = (self.price * self.quantity + other.price * other.quantity)
+        return Grass("Сумма товаров", "Сумма товаров", total_price, total_quantity, "", "", "")
+
+
 category1 = Category("Электроника", "Девайсы и гаджеты")
 product1 = Product("Ноутбук", "Высокопроизводительный ноутбук с SSD", 1200.50, 10)
 product2 = Product("Смартфон", "Последняя модель с OLED-дисплеем", 800.99, 20)
 
+category1.add_product(product1)
+category1.add_product(product2)
 
 products_info = category1.get_products_info()
 for info in products_info:
     print(info)
 
 result_product = product1 + product2
-print( result_product)
+print(result_product)
